d2516ced0aa77fd4bc3df63c0fdf7da4fc0f8716
ibrahim-demirci <ibrahim.demirci@marun.edu.tr>
2021-11-16 22:24:56 +0300

    stream controllers created

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: WeatherApp(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
     );
   }
 }
