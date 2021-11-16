78364cc03be95119ab941e7fea16407ed1c9f3ce
ibrahim-demirci <ibrahim.demirci@marun.edu.tr>
2021-11-16 22:29:55 +0300

    map event to state added

--- lib/bloc/counter/counter_bloc.dart
+++ lib/bloc/counter/counter_bloc.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:repository_pattern_with_bloc/bloc/counter/counter_event.dart';
 
 class CounterBloc {
-  int _counter = 0;
+  int _count = 0;
 
   // StreamController for state
   final _counterStateStreamController = StreamController<int>();
@@ -14,4 +14,17 @@ class CounterBloc {
   final _counterEventStreamController = StreamController<CounterEvent>();
   Stream<CounterEvent> get _counterEventStream => _counterEventStreamController.stream;
   StreamSink<CounterEvent> get counterEventSink => _counterEventStreamController.sink;
+
+  CounterBloc() {
+    _counterEventStream.listen(_mapEventToState);
+  }
+
+  void _mapEventToState(CounterEvent event) {
+    if (event is IncreaseEvent) {
+      _count++;
+    } else {
+      _count--;
+    }
+    _counterStateSink.add(_count);
+  }
 }
